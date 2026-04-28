54a0d7c716ef38dcef81cd68026949ed3fef79eb
dmbiel <dmytro.bieliaiev@gmail.com>
2026-04-28 11:54:38 +0200

    test: add regression coverage for Modal Dialogs

--- src/pages/modal-dialogs.page.ts
+++ src/pages/modal-dialogs.page.ts
@@ -12,12 +12,19 @@ export class ModalDialogsPage {
     await openDemoQaPage(this.page, '/modal-dialogs');
     await expectDemoQaContentPageReady(this.page, {
       heading: 'Modal Dialogs',
-      primaryControls: [this.page.locator('#showSmallModal')],
+      primaryControls: [
+        this.page.locator('#showSmallModal'),
+        this.page.locator('#showLargeModal'),
+      ],
     });
   }
 
   async openSmallModal(): Promise<void> {
     await this.page.locator('#showSmallModal').click();
+  }
+
+  async openLargeModal(): Promise<void> {
+    await this.page.locator('#showLargeModal').click();
   }
 
   async expectSmallModalVisible(): Promise<void> {
@@ -30,8 +37,22 @@ export class ModalDialogsPage {
     );
   }
 
+  async expectLargeModalVisible(): Promise<void> {
+    const modal = this.page.locator('.modal-content');
+
+    await expect(modal).toBeVisible();
+    await expect(modal.locator('.modal-title')).toHaveText('Large Modal');
+    await expect(modal.locator('.modal-body')).toContainText(
+      'Lorem Ipsum is simply dummy text',
+    );
+  }
+
   async closeSmallModal(): Promise<void> {
     await this.page.locator('#closeSmallModal').click();
+  }
+
+  async closeLargeModal(): Promise<void> {
+    await this.page.locator('#closeLargeModal').click();
   }
 
   async expectModalClosed(): Promise<void> {
